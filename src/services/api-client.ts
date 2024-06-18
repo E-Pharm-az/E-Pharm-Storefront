1cddbc80bfd90ebd6cd051ac3d2b296e745c6860
mz-mm <muhammed.mammadhuseynov@gmail.com>
2024-06-18 21:20:41 +0400

    Change base url

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = "https://localhost:7074/api";
-// const BASE_URL = "https://api.e-pharm.co/api";
+// const BASE_URL = "https://localhost:7074/api";
+const BASE_URL = "https://api.e-pharm.co/api";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,

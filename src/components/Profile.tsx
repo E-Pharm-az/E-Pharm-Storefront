bb391cc9a16f684e42773e6c7b00b0e9fdcef92b
mz-mm <muhammed.mammadhuseynov@gmail.com>
2024-09-11 12:26:58 +0300

    Add order history changes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState, useContext } from "react";
 import AuthContext from "../context/AuthProvider.tsx";
-import { LogOut, User } from "lucide-react";
+import { LogOut, Pill, User } from "lucide-react";
 import { Button } from "@/components/ui/button.tsx";
 import useAxiosPrivate from "@/hooks/useAxiosPrivate.ts";
 import { AxiosError } from "axios";
 import { useTranslation } from "react-i18next";
 import ErrorContext from "@/context/ErrorProvider.tsx";
 import { Product } from "@/types/product.ts";
+import { formatPrice } from "@/utils/priceUtils.ts";
 
 interface OrderProduct {
   product: Product;
@@ -18,7 +19,7 @@ interface Order {
   id: number;
   orderProducts: OrderProduct[];
   trackingId: string;
-  orderStatus: string;
+  status: string;
   totalPrice: number;
   address: string;
   district: string;
@@ -32,6 +33,27 @@ const Profile = () => {
   const { setError } = useContext(ErrorContext);
   const [orders, setOrders] = useState<Order[]>();
   const axiosPrivate = useAxiosPrivate();
+
+  const mapStatus = (status: string) => {
+    switch (status) {
+      case "PENDING":
+        return t("orders.status.pending");
+      case "CONFIRMED":
+        return t("orders.status.confirmed");
+      case "REJECTED":
+        return t("orders.status.rejected");
+      case "SHIPPED":
+        return t("orders.status.shipped");
+      case "DELIVERED":
+        return t("orders.status.delivered");
+      case "CANCELED":
+        return t("orders.status.cancelled");
+      case "RETURNED":
+        return t("orders.status.returned");
+      default:
+        return t("orders.status.pending");
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -52,27 +74,75 @@ const Profile = () => {
   return (
     <div className="grid md:flex gap-4">
       <div className="w-full">
-        <h1 className="mb-4 text-xl font-medium">Your orders</h1>
+        <h1 className="mb-4 text-xl font-medium">{t("orders.title")}</h1>
         <div className="space-y-2">
           {orders ? (
-            <div className="p-4 grid gap-2 border rounded-md">
-              {orders.map((order) => (
-                <div key={order.id} className="flex gap-2 border p-2">
-                  {order.orderProducts.map((orderProduct) => (
-                    <p key={orderProduct.product.id}>
-                      {orderProduct.product.name}
-                    </p>
-                  ))}
+            orders.map((order) => (
+              <div key={order.id} className="border p-8 rounded-md grid gap-2">
+                <div className="bg-muted p-1 rounded-md">
+                  <p className="text-center font-md">
+                    {mapStatus(order.status)}
+                  </p>
                 </div>
-              ))}
-            </div>
+                <div className="w-full flex gap-2">
+                  <div className="w-full">
+                    {order.orderProducts.map((orderProduct) => (
+                      <div className="flex gap-2 items-center">
+                        {orderProduct.product.imageUrl ? (
+                          <img
+                            className="w-24 h-24 rounded-md"
+                            src={orderProduct.product.imageUrl}
+                            alt="Product image"
+                          />
+                        ) : (
+                          <div className="w-24 h-24 rounded-md flex items-center justify-center">
+                            <Pill className="text-xl" />
+                          </div>
+                        )}
+                        <div>
+                          <p className="text-lg font-medium">
+                            {orderProduct.product.name}
+                          </p>
+                          <p className="text-sm">
+                            {t("orders.qty")}: {orderProduct.quantity}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="w-[250px] text-lg grid gap-3 mr-auto">
+                    <div>
+                      <p className="text-muted-foreground">
+                        {t("orders.tracking-number")}
+                      </p>
+                      <p>{order.trackingId}</p>
+                    </div>
+                    <div>
+                      <p className="text-muted-foreground">
+                        {t("orders.shipping-address")}
+                      </p>
+                      <p>{order.address}</p>
+                      <p>{order.district}</p>
+                      <p>{order.city}</p>
+                      <p>{order.zip}</p>
+                    </div>
+                    <div>
+                      <p className="text-muted-foreground">
+                        {t("orders.total-price")}
+                      </p>
+                      <p>{formatPrice(order.totalPrice)} ₼</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
           ) : (
-            <p>No orders</p>
+            <p>{t("orders.no-orders")}</p>
           )}
         </div>
       </div>
       <div>
-        <h1 className="mb-4 text-xl font-medium">Your profile</h1>
+        <h1 className="mb-4 text-xl font-medium">{t("profile.title")}</h1>
         <div className="w-full h-min md:w-min border rounded-md p-4">
           <div className="flex items-center justify-between gap-4">
             <div className="flex items-center gap-4">
